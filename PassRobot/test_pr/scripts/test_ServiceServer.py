9e4b2a58240026154eabab9c6200894800772352
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-30 09:23:04 +0000

    Make service server for test

---
+++ PassRobot/test_pr/scripts/test_ServiceServer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from std_srvs.srv import Trigger, TriggerResponse
+
+def serviceServer(srv):
+    res = TriggerResponse()
+    res.success = True
+    res.message = "I receive service client"
+    return res
+
+if __name__ == "__main__":
+    rospy.init_node("server")
+    server = rospy.Service("service",Trigger,serviceServer)
+    print("Service Server start!")
+    rospy.spin()
