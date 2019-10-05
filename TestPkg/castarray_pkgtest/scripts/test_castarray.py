bc286ca702003d8cab9530caa42122af2dabbee4
Besshy <toshiki-yasuda0113@keio.jp>
2019-10-05 03:02:25 +0000

    クラスで書き直した

--- TestPkg/castarray_pkgtest/scripts/test_castarray.py
+++ TestPkg/castarray_pkgtest/scripts/test_castarray.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
+#encoding: utf8
+
 import rospy
 from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import Twist
 
-def callback(msg):
-    print(msg.data)
-    print(msg.data[0])
-    print(msg.data[1])
-    m = Twist()
-    m.linear.x = msg.data[0]
-    m.angular.z = msg.data[1]
-    pub.publish(m)
+class MultiArrayConverter():
+    def __init__(self):
+        self.pub_cmdvel = rospy.Publisher("cmd_vel",Twist,queue_size=10)
+        self.sub_cmdvel = rospy.Subscriber("array",Float32MultiArray,self.to_cmdvel)
+        ## euler角をSubscribeするインスタンスの生成
+
+    def to_cmdvel(self,msg):
+        m = Twist()
+        m.linear.x = msg.data[0]
+        m.angular.z = msg.data[1]
+        print("I receive multiarray")
+        self.pub_cmdvel.publish(m)
+        return 0
+
+    ## オイラー角を配信するcallback関数
+    def to_euler():
+        return 0
+
 
 if __name__ == "__main__":
     rospy.init_node("multiarrayConverter")
-    sub = rospy.Subscriber("array",Float32MultiArray,callback)
-    pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
+    conv = MultiArrayConverter()
     rospy.spin()
