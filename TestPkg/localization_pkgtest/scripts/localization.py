b71e79f9cf9070163a080273f734a3dda662a485
Besshy <toshiki-yasuda0113@keio.jp>
2019-11-30 06:17:21 +0000

    クラス化、メソッド追加

--- TestPkg/localization_pkgtest/scripts/localization.py
+++ TestPkg/localization_pkgtest/scripts/localization.py
@@ -4,35 +4,50 @@ import rospy
 from std_msgs.msg import Float32MultiArray
 import math
 
-def updateInput(msg):
-    x = msg.data[0]
-    theta = msg.data[1]
-    theta = 0
-    delta_r_a = 0
-    delta_theta_a = 0
-    r_a = 0
-    l = 500
-    x_a = r_a - l*math.cos(theta)
-    y_a = r_a - l*math.sin(theta)
-    theta_a = theta_a + delta_theta_a
-    '''
-    r_a = r_a + delta_r_a * ((math.cos(theta + (theta_a - delta_theta_a / 2.0) + math.pi / 2)),
-                                math.sin(theta + (theta_a - delta_theta_a / 2.0)))
-    '''
-    x_a = x_a + delta_r_a * math.cos(theta) * (math.cos(theta + (theta_a - delta_theta_a / 2.0) + math.pi))
-    y_a = y_a + delta_r_a * math.sin(theta) * (math.sin(theta + (theta_a - delta_theta_a / 2.0) + math.pi))
+class Localization():
+    
+    def __init__(self):
+        
 
-    print("-------------")
-    print(x)
-    print(theta)
-    calcLocalization(x_a,y_a,0,0,0,0,0,0)
+    def right_forward(msg):
+        x = msg.data[0]
+        theta = msg.data[1]
+        theta = 0
+        delta_r_a = 0
+        delta_theta_a = 0
+        r_a = 0
+        l = 500
+        x_a = r_a - l*math.cos(theta)
+        y_a = r_a - l*math.sin(theta)
+        theta_a = theta_a + delta_theta_a
+        '''
+        r_a = r_a + delta_r_a * ((math.cos(theta + (theta_a - delta_theta_a / 2.0) + math.pi / 2)),
+                                    math.sin(theta + (theta_a - delta_theta_a / 2.0)))
+        '''
+        x_a = x_a + delta_r_a * math.cos(theta) * (math.cos(theta + (theta_a - delta_theta_a / 2.0) + math.pi))
+        y_a = y_a + delta_r_a * math.sin(theta) * (math.sin(theta + (theta_a - delta_theta_a / 2.0) + math.pi))
 
-def calcLocalization(x_a,y_a,x_b,y_b,x_c,y_c,x_d,y_d):
-    x_0 = (x_a + x_b + x_c + x_d) / 4
-    y_0 = (y_a + y_b + y_c + y_d) / 4
-    theta = (math.atan(y_a - y_0 / x_a - x_0) + math.atan(y_b - y_0 / x_b - x_0) 
-            + math.atan(y_c - y_0 / x_c - x_0) + math.atan(y_d - y_0 / x_d - x_0)) / 4
-    return x_0, y_0, theta
+        print("-------------")
+        print(x)
+        print(theta)
+        return x_a, y_a
+
+    def left_forword():
+        return 0
+
+    def right_back():
+        return 0
+
+    def left_back():
+        return 0
+
+    def update(x_a,y_a,x_b,y_b,x_c,y_c,x_d,y_d):
+        right_front()
+        x_0 = (x_a + x_b + x_c + x_d) / 4
+        y_0 = (y_a + y_b + y_c + y_d) / 4
+        theta = (math.atan(y_a - y_0 / x_a - x_0) + math.atan(y_b - y_0 / x_b - x_0) 
+                + math.atan(y_c - y_0 / x_c - x_0) + math.atan(y_d - y_0 / x_d - x_0)) / 4
+        return x_0, y_0, theta
 
 if __name__ == "__main__":
     rospy.init_node("Localization")
