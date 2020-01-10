092695943cb08302df83985f211fdbb4b54876f7
ilias-taro-nuc <rogikenilias@gmail.com>
2020-01-11 01:19:26 +0900

    create joystick_tr package

---
+++ TryRobot/joystick_tr/src/joystick_tr.cpp
@@ -0,0 +1,57 @@
+#include "ros/ros.h"
+#include "std_msgs/Float32MultiArray.h"
+#include "std_msgs/Int32MultiArray.h"
+
+#include "sensor_msgs/Joy.h"
+
+float joystick_R[2] = {0.0, 0.0};
+int button[4] = {0, 0, 0, 0};
+
+void msgCallback(const sensor_msgs::Joy &msg)
+{
+    joystick_R[0] = -msg.axes[2];
+    joystick_R[1] = msg.axes[3];
+    for (int i = 0; i < 4; i++)
+    {
+        button[i] = msg.buttons[i];
+    }
+}
+
+std_msgs::Float32MultiArray pub_multi_float(float *, int);
+std_msgs::Int32MultiArray pub_multi_int(int *, int);
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "joystick_tr_node");
+
+    ros::NodeHandle n;
+    ros::Publisher multifloat_pub = n.advertise<std_msgs::Float32MultiArray>("control_float", 10);
+    ros::Publisher multiint_pub = n.advertise<std_msgs::Int32MultiArray>("control_int", 10);
+
+    ros::Subscriber joy_sub = n.subscribe("joy", 100, msgCallback);
+
+    ros::NodeHandle arg_n("~");
+    int looprate = 30; // Hz
+    arg_n.getParam("loop", looprate);
+
+    ros::Rate loop_rate(looprate);
+    while (ros::ok())
+    {
+        std_msgs::Float32MultiArray floatarray;
+        floatarray.data.resize(2);
+        floatarray.data[0] = joystick_R[0] * 100;
+        floatarray.data[1] = joystick_R[1] * 100;
+        multifloat_pub.publish(floatarray);
+
+        std_msgs::Int32MultiArray intarray;
+        intarray.data.resize(4);
+        for (int i = 0; i < 4; i++)
+        {
+            intarray.data[i] = button[i];
+        }
+        multiint_pub.publish(intarray);
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
